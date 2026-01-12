9b66942bbaac2671a07211a76c97541bbfbceb97
Ronaldo Junior <152401171+Dev-RonaldoJunior@users.noreply.github.com>
2026-01-12 09:47:26 -0300

    Adicionar tratamento de formulário de login via POST e atualizar a página de login
    A rota /login agora suporta requisições POST e processa os campos de e-mail e senha enviados. O template login.html foi atualizado para incluir um formulário para entrada de dados do usuário.

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        senha = request.form['senha']
+
+        return f"Email recebido: {email} e senha recebida: {senha}"
+    
     return render_template('login.html')
 
 if __name__ == '__main__':
